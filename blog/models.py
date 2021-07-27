3be42334b6e2c625dc1e75fd4560dd1d51aded48
Helder Nascimento <helder.nascimento@ext.vallourec.com>
2021-07-27 12:26:58 +0200

    fix: add new bootstrap and new login route

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -8,7 +9,8 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-
+    
+    @login_required
     def publish(self):
         self.published_date = timezone.now()
         self.save()
